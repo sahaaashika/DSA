48d1647b680d36c92263d7d9e06ce7d53b66fac3
Aashika Saha <sahaaashika@gmail.com>
2026-08-22 00:49:09 +0530

    Time: 6 ms (5.29%) | Memory: 43.2 MB (57.82%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='{' || c=='[' || c=='('){
+                stack.push(c);
+            }
+            else if(c=='}'){
+                if(stack.isEmpty() || stack.pop()!='{')
+                return false;
+            } 
+            else if(c==')'){
+                if(stack.isEmpty() || stack.pop()!='(')
+                return false;
+            } 
+            else {
+                if(stack.isEmpty() || stack.pop()!='[')
+                return false;
+            } 
+        }
+        if(stack.isEmpty())
+                return true;
+        else
+        return false;
+
+    }
+}
