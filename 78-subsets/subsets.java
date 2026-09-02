0a6fba255dd7eae7ecc7b85cea47e192c2799ea9
Aashika Saha <sahaaashika@gmail.com>
2026-09-02 23:22:16 +0530

    Time: 1 ms (86.71%) | Memory: 44 MB (75.36%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n= nums.length;
+        for(int mask=0;mask<Math.pow(2,n);mask++){
+
+            List<Integer> subset = new ArrayList<>();
+            for(int i=0;i<n;i++){
+             if ((mask & (1 << i)) != 0) {
+
+                    subset.add(nums[i]);
+                }
+            }
+
+            result.add(subset);
+        }
+
+        return result;
+    }
+}
